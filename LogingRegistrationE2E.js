7ea07252efdd84941b9b118838f1d79f50940e32
Yorkshire <ocallejas@rocketlawyer.com>
2014-12-05 13:43:28 -0800

    Create LogingRegistrationE2E.js

---
+++ LogingRegistrationE2E.js
@@ -0,0 +1,18 @@
+casper.test.begin('Login and Registration End to End Test', function suite(test) {
+
+    // Load the landing page and verify it is correct
+    var startingUrl = getStartingPoint("/homepage.rl");
+    casper.start( startingUrl, function() {
+        this.echo("======= Beginning Test Suite =======");
+        test.assertHttpStatus(200, 'Connected to Landing page');
+        test.assertTitle('Affordable Legal Services, Free Legal Documents, Advice & Ask a Lawyer | Rocket Lawyer', 'Title is correct');
+    });
+
+ casper.thenClick(".rlHeaderTopRightLink[href='/login-register.rl#/register?hd=navreg']", function () {
+  this.echo("\nGoing to the registration page \n", "COMMENT");
+});
+
+    casper.run(function() {
+        test.done();
+    });
+});
